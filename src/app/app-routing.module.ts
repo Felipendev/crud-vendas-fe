af1cbbc49b0e35871507c978f41f800a1cfbf912
Felipendev <felipe.psic@outlook.com.br>
2022-08-11 09:02:07 -0300

    implementa pagina not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'vendas'},
   {
     path: 'vendas',
     loadChildren: () => import('./vendas/vendas.module').then(m => m.VendasModule)
-  }
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+    data: {
+      title: 'Not Found'
+    }
+},
+{
+    path: '**',
+    redirectTo: 'not-found'
+}
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
